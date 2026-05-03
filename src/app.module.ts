93304b9205042ab3a7da4b090d6e5d2dfecf39f4
Your Name <mohammadnaved1352@gmail.com>
2026-05-03 21:27:39 +0530

    Fix: use REDIS_URL for Railway

--- src/app.module.ts
+++ src/app.module.ts
@@ -25,7 +25,7 @@ import { redisStore } from 'cache-manager-redis-yet';
       store:await redisStore({
         // 👆 Redis ko store ki tarah use karo
         socket:{
-          host:ConfigService.get<string>('REDIS_HOST'),
+          host:ConfigService.get<string>('REDIS_URL'),
           port:ConfigService.get<number>('REDIS_PORT')
         },
       }),
